1f2082bcc74fee03581b073dea5717ecff312a8a
katy@karstaway.com <katy@karstaway.com>
2018-07-02 14:14:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ function prependKitten(name) {
 }
 
 function removeLastKitten(name) {
-   var newKittens = kittens.slice (kittens.length - 1)
+   var newKittens = kittens.slice (0, kittens.length - 1)
   return newKittens
   
 }
